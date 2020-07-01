21bd829e20c9b41f5e5a92d36b640046f6921562
Raafa1993 <rafaelajds@gmail.com>
2020-07-01 00:20:22 -0300

    create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './App.css';
+
+import Primeiro from './components/layout/Primeiro'
+import ComParametro from './components/layout/ComParametro'
+import ComFilhos from './components/layout/ComFilhos'
+import Cards from './components/layout/Cards'
+
+export default (props) => (
+  <div className="App">
+    <Cards titulo="#03 - Componente com Filhos">
+      <ComFilhos>
+        <ul>
+          <li>Rafa</li>
+          <li>Teus</li>
+          <li>Japa</li>
+          <li>Dan</li>
+        </ul>
+      </ComFilhos>
+    </Cards>
+    <Cards titulo="#02 - Componente com Parametro">
+      <ComParametro titulo="Esse é o titulo"
+        subtitulo="Esse é o subtituo" />
+    </Cards>
+    <Cards titulo="#01 - Primeiro Componente">
+      <Primeiro />
+    </Cards>
+  </div>
+)
--- src/index.js
+++ src/index.js
@@ -2,30 +2,9 @@ import './index.css';
 import React from 'react'
 import ReactDom from 'react-dom'
 
-import Primeiro from './components/Primeiro'
-//import ComParametro from './components/ComParametro'
-//import ComFilhos from './components/ComFilhos'
-import Cards from './components/layout/Cards'
+import App from './App'
 
 ReactDom.render(
-    <div>
-        <Cards titulo="Exercicio X">
-            <Primeiro />
-        </Cards>
-        <Cards titulo="Exercicio X">
-            Conteudo
-        </Cards>
-        {/* <ComFilhos>
-            <ul>
-                <li>Rafa</li>
-                <li>Teus</li>
-                <li>Japa</li>
-                <li>Dan</li>
-            </ul>
-        </ComFilhos> */}
-        {/* <Primeiro />
-        <ComParametro titulo="Esse é o titulo"
-            subtitulo="Esse é o subtituo" /> */}
-    </div>,
+    <App></App>,
     document.getElementById('root')
 )
